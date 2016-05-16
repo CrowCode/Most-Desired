7ad9bd27e594feb1f017515f672d43b43fecdcac
sajjad <s.hassanpourk@gmail.com>
2016-05-16 23:23:54 +0200

    Finil state

--- MostDesired/src/view/sajjad/mostdesired/Intro.java
+++ MostDesired/src/view/sajjad/mostdesired/Intro.java
@@ -3,12 +3,15 @@ package view.sajjad.mostdesired;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.HeadlessException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
@@ -19,14 +22,13 @@ public class Intro extends JFrame {
 
 	private Color myOrange = new Color(240, 127, 7);
 	private Color myCyan = new Color(60, 109, 130);
-	private Color darkGray = new Color(55, 55, 55);
 
 	private JProgressBar pBar;
 	private Timer timer;
 
 	public Intro() throws HeadlessException {
 
-		setSize(400, 300);
+		setSize(620, 300);
 		setLayout(new BorderLayout());
 		setUndecorated(true);
 		setLocationRelativeTo(null);
@@ -57,7 +59,7 @@ public class Intro extends JFrame {
 	}
 
 	private void setupTimer() {
-		timer = new Timer(500, listener);
+		timer = new Timer(1500, listener);
 		timer.start();
 	}
 
@@ -78,49 +80,23 @@ public class Intro extends JFrame {
 
 class IntroPanel extends JPanel {
 	
-	private Color myOrange = new Color(240, 127, 7);
-	private Color myCyan = new Color(60, 109, 130);
-	private Color darkGray = new Color(55, 55, 55);
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	@Override
 	public void paint(Graphics g) {
 		
-		g.setFont(new Font("Garamond", Font.BOLD , 40));
-		g.setColor(myOrange);
-		
-		g.drawString("M", 90, 100);
-		
-		g.setFont(new Font("Garamond", Font.BOLD , 20));
-		g.setColor(myCyan);
-		
-		g.drawString("ost", 125, 100);
-		
-		g.setFont(new Font("Garamond", Font.BOLD , 60));
-		g.setColor(myOrange);
-		
-		g.drawString("D", 180, 120);
-
-		g.setFont(new Font("Garamond", Font.BOLD , 30));
-		g.setColor(myCyan);
-		
-		g.drawString("e", 220, 120);
-		g.setColor(darkGray);
-		g.setFont(new Font("Garamond", Font.BOLD , 50));
-		
-		g.drawString("s", 233, 125);
-		
-		g.setFont(new Font("Garamond", Font.BOLD , 30));
-		
-		g.drawString("i", 259, 120);
-		
-		g.drawString("r", 265, 120);
-		g.setColor(myCyan);
-		g.drawString("e", 277, 120);
-		g.setColor(darkGray);
-		g.setFont(new Font("Garamond", Font.BOLD , 50));
-		
-		g.drawString("d", 290, 120);
-		
+		BufferedImage img;
+		try {
+			img = ImageIO.read(new File("Thesis_App_startup.jpg"));
+			g.drawImage(img, 0, 0, null);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
 		
 	}
 	
